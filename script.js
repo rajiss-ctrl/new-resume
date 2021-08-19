fcd3a95d39ac98c8ce1364bae9ac73fde22a184d
Raji Sarafadeen Sanjo <omosanjos77@gmail.com>
2021-08-19 21:57:26 +0100

    movr on

---
+++ script.js
@@ -0,0 +1,7 @@
+let first_name = "Raji";
+let last_name = "Sarafadeen Sanjo";
+
+const fullName = `My name is:- ${first_name} ${last_name}`;
+
+console.log(fullName)
+alert(fullName.toLocaleUpperCase())
